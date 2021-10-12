b542624118d0ea095dabb23f15da92fdd3f8ae56
Vincent <vincent.dehouve@supbioteh.fr>
2021-10-12 10:27:11 +0200

    Exo 3) + 1 client

--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import metier.Adresse;
 import metier.Boutique;
+import metier.Client;
 import metier.Console;
 import metier.Jeu;
 
@@ -32,6 +33,33 @@ public class Test {
 	Jeu j1 = new Jeu("F1 2021", Consoles);
 	Jeu j2 = new Jeu("Fifa 21", Consoles);
 	Jeu j3 = new Jeu("Call of duty", Consoles);
+	
 
-	}
+	Console playstation = new Console ("Playstation");
+	Console gameboy = new Console ("gameboy");
+	Console xbox = new Console ("xbox");
+	Console wii = new Console ("wii");
+	
+	List <Console> listConsole = new ArrayList();
+	listConsole.add(playstation);
+	listConsole.add(gameboy);
+	
+	
+	List <Console> listConsole1 = new ArrayList();
+	listConsole.add(wii);
+	listConsole.add(xbox);
+	
+	List <Console> listConsole2 = new ArrayList();
+	listConsole.add(gameboy);
+	listConsole.add(xbox);
+	Jeu j = new Jeu("Pokemon",listConsole);
+	Jeu j4 = new Jeu("Mario",listConsole1);
+	Jeu j5 = new Jeu("Zelda",listConsole2);
+	
+	
+	
+	Client c = new Client ("Vince","Dve","Pokemon");
+}
+
+	
 }
