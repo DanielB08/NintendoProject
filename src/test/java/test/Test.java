6eecd073999a4114bd0280dfd57857462e225732
Dorian Valery <d.valery12@gmail.com>
2021-10-12 10:28:27 +0200

    Modif test

--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -12,26 +12,30 @@ import metier.Jeu;
 public class Test {
 
 	public static void main(String[] args) {
-		
-		List<Jeu> jeux = new ArrayList();
+
 		List<Console> Consoles = new ArrayList();
-		
-		Adresse adresse = new Adresse (3, 4, "Paris");
-	
-		Boutique boutique = new Boutique("Micromania", adresse,jeux);
-		
-		
 		Console c1 = new Console("Nintendo Gamecube");
 		Console c2 = new Console("PlayStation4");
 		Console c3 = new Console("Xbox one");
-		
+
 		Consoles.add(c1);
 		Consoles.add(c2);
 		Consoles.add(c3);
-	
-	Jeu j1 = new Jeu("F1 2021", Consoles);
-	Jeu j2 = new Jeu("Fifa 21", Consoles);
-	Jeu j3 = new Jeu("Call of duty", Consoles);
+
+		Jeu j1 = new Jeu("F1 2021", Consoles);
+		Jeu j2 = new Jeu("Fifa 21", Consoles);
+		Jeu j3 = new Jeu("Call of duty", Consoles);
+
+		List<Jeu> jeux = new ArrayList();
+		jeux.add(j1);
+		jeux.add(j2);
+		jeux.add(j3);
+
+		Adresse adresse = new Adresse (3, 4, "Paris");
+
+		Boutique boutique = new Boutique("Micromania", adresse,jeux);
+
+
 
 	}
 }
