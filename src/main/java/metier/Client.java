95879cc6c64666688ea1b77b416603190c3ffbd3
Fabiola <fabiola.ouedraogo@gmail.com>
2021-10-12 09:46:45 +0200

    Modif client

--- src/main/java/metier/Client.java
+++ src/main/java/metier/Client.java
@@ -2,8 +2,47 @@ package metier;
 
 public class Client {
 
-	public Client() {
-		// TODO Auto-generated constructor stub
-	}
+		private String name;
+		private String prenom;
+		private String listeDeJeu;
+		
+		public Client() {
+			
+		}
 
+		public Client(String name, String prenom, String listeDeJeu) {
+			super();
+			this.name = name;
+			this.prenom = prenom;
+			this.listeDeJeu = listeDeJeu;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getPrenom() {
+			return prenom;
+		}
+
+		public void setPrenom(String prenom) {
+			this.prenom = prenom;
+		}
+
+		public String getListeDeJeu() {
+			return listeDeJeu;
+		}
+
+		public void setListeDeJeu(String listeDeJeu) {
+			this.listeDeJeu = listeDeJeu;
+		}
+
+		@Override
+		public String toString() {
+			return "Client [name=" + name + ", prenom=" + prenom + ", listeDeJeu=" + listeDeJeu + "]";
+		}
 }
