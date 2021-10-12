a6ec2880d1b0d1093f299133c3d0c72a56888b49
Daniel <daniel.bilal.08@gmail.com>
2021-10-12 10:16:57 +0200

    AddConsle+Jeu

--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -5,15 +5,33 @@ import java.util.List;
 
 import metier.Adresse;
 import metier.Boutique;
+import metier.Console;
 import metier.Jeu;
 
 
 public class Test {
 
 	public static void main(String[] args) {
+		
+		List<Jeu> jeux = new ArrayList();
+		List<Console> Consoles = new ArrayList();
+		
 		Adresse adresse = new Adresse (3, 4, "Paris");
-		List<Jeu> jeu = new ArrayList();
-		Boutique boutique = new Boutique("Micromania", adresse,jeu);
-	}
+	
+		Boutique boutique = new Boutique("Micromania", adresse,jeux);
+		
+		
+		Console c1 = new Console("Nintendo Gamecube");
+		Console c2 = new Console("PlayStation4");
+		Console c3 = new Console("Xbox one");
+		
+		Consoles.add(c1);
+		Consoles.add(c2);
+		Consoles.add(c3);
+	
+	Jeu j1 = new Jeu("F1 2021", Consoles);
+	Jeu j2 = new Jeu("Fifa 21", Consoles);
+	Jeu j3 = new Jeu("Call of duty", Consoles);
 
+	}
 }
